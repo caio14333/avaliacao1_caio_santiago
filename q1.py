64c97dbaba77d36a720757dda2b7026523b5d494
unknown <anaz.maiaf@gmail.com>
2025-09-02 10:32:36 -0300

    questoes resolvidas

---
+++ q1.py
@@ -0,0 +1,12 @@
+#questao 1 
+numero = int(input("digite um numero inteiro"))
+if numero % 2 == 0:
+    print("esse numero é par")
+else:
+    print("esse nuemro é impar")
+if numero == 0:
+    print("esse numero é zero")
+if numero >=1:
+    print("esse numero é positivo")
+if numero <0:
+    print("esse numero é negativo")
---
+++ q2.py
@@ -0,0 +1,18 @@
+#questao 2
+num_produtos = int(input("Quantos produtos foram vendidos? "))
+total_vendas = 0
+contagem_maior_50 = 0
+contagem_menor_50 = 0
+for i in range(num_produtos):
+    while True: 
+            valor_produto = float(input(f"Digite o valor do produto {i+1}: R$ "))
+            break 
+    total_vendas += valor_produto
+    if valor_produto > 50:
+        contagem_maior_50 += 1
+    else:
+        contagem_menor_50 += 1
+print("\n--- Resumo das Vendas ---")
+print(f"Valor total dos produtos vendidos: R$ {total_vendas:.2f}")
+print(f"Quantidade de produtos com valor > 50 reais: {contagem_maior_50}")
+print(f"Quantidade de produtos com valor < 50 reais: {contagem_menor_50}")
