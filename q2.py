6f4930a3d31796c43a1df661e0749c9aa2682836
unknown <anaz.maiaf@gmail.com>
2025-09-02 10:42:20 -0300

    questoes resolvidas

--- q2.py
+++ q2.py
@@ -8,11 +8,11 @@ for i in range(num_produtos):
             valor_produto = float(input(f"Digite o valor do produto {i+1}: R$ "))
             break 
     total_vendas += valor_produto
-    if valor_produto > 50:
+    if valor_produto >= 50:
         contagem_maior_50 += 1
     else:
         contagem_menor_50 += 1
 print(" Resumo das Vendas")
 print(f"Valor total dos produtos vendidos: R$ {total_vendas:.2f}")
-print(f"Quantidade de produtos com valor > 50 reais: {contagem_maior_50}")
+print(f"Quantidade de produtos com valor > ou igual a 50 reais: {contagem_maior_50}")
 print(f"Quantidade de produtos com valor < 50 reais: {contagem_menor_50}")
